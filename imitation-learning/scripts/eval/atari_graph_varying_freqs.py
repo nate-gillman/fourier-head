e899e5af04442e241cc4be5b2d3a63695117c2f4
Nate Gillman <80133994+nate-gillman@users.noreply.github.com>
2024-11-14 22:08:25 -0500

    atari_graph_varying_freqs now works for all games

--- imitation-learning/scripts/eval/atari_graph_varying_freqs.py
+++ imitation-learning/scripts/eval/atari_graph_varying_freqs.py
@@ -3,13 +3,22 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import numpy as np
 import json
+import argparse
+
+from atari_expert_scores import get_human_normalized_score
 
 font_path = 'scripts/eval/Times_New_Roman.ttf'
 font_manager.fontManager.addfont(font_path)
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = 'Times New Roman'
 
-def get_returns_metrics(returns_local):
+def get_frequencies_for_game(game_name):
+    if game_name == 'Seaquest':
+        return list(range(2, 34, 2))  # [2,4,6,8,...,32]
+    else:
+        return list(range(4, 36, 4))  # [4,8,12,...,32]
+
+def get_returns_metrics(game_name, returns_local):
 
     returns_best = []
     for returns in returns_local.values():
@@ -18,7 +27,7 @@ def get_returns_metrics(returns_local):
     returns_best = np.asarray(returns_best)
 
     # normalize
-    returns_best = 100*(returns_best-68)/(42055-68)
+    # returns_best = 100*get_human_normalized_score(game_name.lower(), returns_best)
 
     returns_mean = returns_best.mean()
     returns_std = returns_best.std()
@@ -26,7 +35,7 @@ def get_returns_metrics(returns_local):
     return returns_mean, returns_std
 
 
-def gather_returns_and_smoothness(use_originally_reported_baseline=True):
+def gather_returns_and_smoothness(game_name, use_originally_reported_baseline=True):
 
     DIVIDE_STD_BY = (1/0.67) # this gives 50% confidence interval
 
@@ -42,14 +51,14 @@ def gather_returns_and_smoothness(use_originally_reported_baseline=True):
     else:
 
         # GET RETURNS STATISTICS
-        returns_local_path = f"output/linear/returns_dict.json"
+        returns_local_path = f"output/{game_name}/linear/returns_dict.json"
         with open(returns_local_path, "r") as f:
             returns_local = json.load(f)
-        return_baseline, return_std = get_returns_metrics(returns_local)
+        return_baseline, return_std = get_returns_metrics(game_name, returns_local)
         return_baseline_err = return_std / DIVIDE_STD_BY
 
         # GET SMOOTHNESS STATISTICS
-        smoothness_local_path = f"output/linear/smoothness_dict.json"
+        smoothness_local_path = f"output/{game_name}/linear/smoothness_dict.json"
         with open(smoothness_local_path, "r") as f:
             smoothness_local = json.load(f)
         smoothness_baseline = smoothness_local["L2"]["mean"]
@@ -68,18 +77,19 @@ def gather_returns_and_smoothness(use_originally_reported_baseline=True):
     # STEP 2: GET FOURIER METRICS
     returns_fourier, returns_fourier_err = [], []
     smoothness_fourier, smoothness_fourier_err = [], []
-    for freqs in [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]:
+    freqs = get_frequencies_for_game(game_name)
+    for freq in freqs:
 
         # GET RETURNS STATISTICS
-        returns_local_path = f"output/fourier_{freqs}/returns_dict.json"
+        returns_local_path = f"output/{game_name}/fourier_{freq}/returns_dict.json"
         with open(returns_local_path, "r") as f:
             returns_local = json.load(f)
-        returns_mean, returns_std = get_returns_metrics(returns_local)
+        returns_mean, returns_std = get_returns_metrics(game_name, returns_local)
         returns_fourier.append(returns_mean)
         returns_fourier_err.append(returns_std)
 
         # GET SMOOTHNESS STATISTICS
-        smoothness_local_path = f"output/fourier_{freqs}/smoothness_dict.json"
+        smoothness_local_path = f"output/{game_name}/fourier_{freq}/smoothness_dict.json"
         with open(smoothness_local_path, "r") as f:
             smoothness_local = json.load(f)
         smoothness_mean = smoothness_local["L2"]["mean"]
@@ -95,10 +105,10 @@ def gather_returns_and_smoothness(use_originally_reported_baseline=True):
         "smoothness_err"    : smoothness_fourier_err
     }
 
-    return vals
+    return vals, freqs
 
 
-def build_graphs(vals, output_fname, title=""):
+def build_graphs(vals, freqs, output_fname, title=""):
 
     return_baseline         = vals["baseline"]["return"]
     return_baseline_err     = vals["baseline"]["return_err"]
@@ -116,19 +126,16 @@ def build_graphs(vals, output_fname, title=""):
     # Global title for both graphs
     fig.suptitle(title, fontsize=24, y=0.95)
 
-    # X-axis values (2, 4, 6, ..., 32)
-    generations = list(range(2, 34, 2))
-
     # Plot the first graph (Returns)
-    ax1.plot(generations, returns_fourier, c="tab:blue", label="Fourier head")
-    ax1.fill_between(generations, 
+    ax1.plot(freqs, returns_fourier, c="tab:blue", label="Fourier head")
+    ax1.fill_between(freqs, 
                      np.array(returns_fourier) - np.array(returns_fourier_err), 
                      np.array(returns_fourier) + np.array(returns_fourier_err), 
                      color="tab:blue", alpha=0.2)  # Standard deviation tunnel for returns
 
     # Add red horizontal line for the baseline and tunnel for the baseline
     ax1.axhline(y=return_baseline, color='red', linestyle='--', label="Linear head")
-    ax1.fill_between(generations, 
+    ax1.fill_between(freqs, 
                      return_baseline - return_baseline_err, 
                      return_baseline + return_baseline_err, 
                      color='red', alpha=0.2)  # Standard deviation tunnel for baseline
@@ -141,15 +148,15 @@ def build_graphs(vals, output_fname, title=""):
     ax1.legend(loc="lower right")
 
     # Plot the second graph (Smoothness)
-    ax2.plot(generations, smoothness_fourier, c="tab:blue", label="Fourier head")
-    ax2.fill_between(generations, 
+    ax2.plot(freqs, smoothness_fourier, c="tab:blue", label="Fourier head")
+    ax2.fill_between(freqs, 
                      np.array(smoothness_fourier) - np.array(smoothness_fourier_err), 
                      np.array(smoothness_fourier) + np.array(smoothness_fourier_err), 
                      color="tab:blue", alpha=0.2)  # Standard deviation tunnel for smoothness
 
     # Add red horizontal line for the baseline and tunnel for the baseline
     ax2.axhline(y=smoothness_baseline, color='red', linestyle='--', label="Linear head")
-    ax2.fill_between(generations, 
+    ax2.fill_between(freqs, 
                      smoothness_baseline - smoothness_baseline_err, 
                      smoothness_baseline + smoothness_baseline_err, 
                      color='red', alpha=0.2)  # Standard deviation tunnel for baseline
@@ -168,15 +175,29 @@ def build_graphs(vals, output_fname, title=""):
 
 def main():
 
-    use_originally_reported_baseline = True
-    vals = gather_returns_and_smoothness(
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'game', 
+        type=str, 
+        help='Game name (e.g. DoubleDunk or Seaquest)'
+    )
+    args = parser.parse_args()
+
+    if args.game == "Seaquest":
+        use_originally_reported_baseline = True
+    else:
+        use_originally_reported_baseline = False
+
+    vals, freqs = gather_returns_and_smoothness(
+        args.game,
         use_originally_reported_baseline=use_originally_reported_baseline
     )
 
     build_graphs(
         vals,
-        output_fname="scripts/eval/atari_graph_varying_freqs.png",
-        title="Impact of Varying Fourier Frequencies on Decision Transformer"
+        freqs,
+        output_fname=f"output/{args.game}/atari_graph_varying_freqs_{args.game}.png",
+        title=f"Impact of Varying Fourier Frequencies on Decision Transformer: {args.game}"
     )
 
 if __name__ == "__main__":
