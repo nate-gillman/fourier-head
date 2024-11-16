07e24536c59eda72d6251f9ff0248f9bb9698ba4
Nate Gillman <80133994+nate-gillman@users.noreply.github.com>
2024-11-16 08:48:24 -0500

    add size scale to Atari

--- imitation-learning/scripts/train/exps_linear.sh
+++ imitation-learning/scripts/train/exps_linear.sh
@@ -2,12 +2,14 @@ echo "exps_linear"
 
 num_steps=500000
 game_name=$1
+scale=$2
 
 for seed in 123 231 312 42
 do
     echo "game_name=$game_name"
     echo "num_steps = $num_steps"
     echo "seed=$seed"
+    echo "scale=$scale"
 
     python run_dt_atari.py \
         --seed $seed \
@@ -19,5 +21,6 @@ do
         --game "$game_name" \
         --batch_size 128 \
         --data_dir_prefix ./dataset/ \
-        --save_path ./output/$game_name/linear/
+        --save_path ./output/$game_name-$scale/linear/ \
+        --scale $scale
 done
