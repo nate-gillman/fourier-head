dcd3d832d2867cea2bf0e2258f87082ae173ca4d
Nate Gillman <80133994+nate-gillman@users.noreply.github.com>
2025-01-27 19:57:55 -0500

    update slurm script

--- toy-example-synthetic/scripts/slurm_1gpu.sh
+++ toy-example-synthetic/scripts/slurm_1gpu.sh
@@ -3,13 +3,13 @@
 # A batch script for running a job on Oscar's 3090 condo, using the Slurm scheduler
 # The 3090 condo runs NVIDIA's GeForce RTX 3090 graphics card
 
-#SBATCH -p 3090-gcondo --gres=gpu:1
+#SBATCH -p gpu-he --gres=gpu:1
 #SBATCH --constraint=a6000|geforce3090|l40s
 #SBATCH --exclude=gpu1506,gpu2108,gpu2114,gpu2115,gpu2116
 #SBATCH -N 1 # gives one node, makes sure cpu cores are on same node
 #SBATCH -c 1 # num CPU cores
 #SBATCH --mem=31G
-#SBATCH -t 6:00:00
+#SBATCH -t 2:00:00
 #SBATCH -e output/slurm_logs/%j.err
 #SBATCH -o output/slurm_logs/%j.out
 #SBATCH --mail-user=nate_gillman@brown.edu
@@ -49,7 +49,7 @@ cd scripts
 # # experiments with regularization
 # sh ./run_exps_fourier_reg.sh gaussian
 # sh ./run_exps_fourier_reg.sh gmm2
-# sh ./run_exps_fourier_no_reg.sh beta
+# sh ./run_exps_fourier_reg.sh beta
 
 # sh ./run_exps_gmm.sh gaussian
 # sh ./run_exps_gmm.sh gmm2
@@ -71,6 +71,6 @@ cd scripts
 
 # sh ./run_exps_gmm-mle.sh gaussian
 # sh ./run_exps_gmm-mle.sh gmm2
-sh ./run_exps_gmm-mle.sh beta
+# sh ./run_exps_gmm-mle.sh beta
 
 
